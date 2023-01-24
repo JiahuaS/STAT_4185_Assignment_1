a7bb698e4724ba17591812ecbb3b6ffb2082f7df
Jiahua Song <jiahuasong@JiahuadeMacBook-Pro.local>
2023-01-23 19:37:37 -0500

    decipher second

--- decrypt_message_two.py
+++ decrypt_message_two.py
@@ -5,3 +5,12 @@ encrypted_message = encrypted_file.readline()
 encrypted_file.close()
 
 # Write Code Here
+split_message = [encrypted_message[i] for i in range(len(encrypted_message))]
+split_message_action = [encrypted_message[i] for i in range(len(encrypted_message))]
+split_message_action.reverse()
+
+for i in range(len(split_message)):
+    if i%2 != 0:
+        split_message[i] = split_message_action[i]
+decrpted_message = ''.join(split_message)
+print(decrpted_message)
